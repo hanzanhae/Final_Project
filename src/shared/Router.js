b3db0187ebb5bd9f96b49056acdc9793c655ec93
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2024-10-16 18:09:15 +0900

    로그인버튼추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -5,6 +5,7 @@ import MainHeader from '../components/MainHeader';
 import User from '../pages/User';
 import AddMeeting from '../pages/AddMeeting';
 import DetailMeeting from '../pages/DetailMeeting';
+import Login from '../pages/Login';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
       <MainHeader />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/user" element={<User />} />
         <Route path="/addMeet" element={<AddMeeting />} />
         <Route path="/detail" element={<DetailMeeting />} />
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 export const lightTheme = {
   bgColor: '#ffffff50',
+  bgColorDark: '#ffffff',
   textColor: '#000000',
   pointColor: '#023EC7',
   pointColorLight: '#e0edff'
@@ -7,6 +8,7 @@ export const lightTheme = {
 
 export const darkTheme = {
   bgColor: '#00000050',
+  bgColorDark: '#000000d6',
   textColor: '#ffffff',
   pointColor: '#3C3D6D',
   pointColorLight: '#ddd9eb'
